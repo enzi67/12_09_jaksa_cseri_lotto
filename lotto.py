1c9a624ed60d99a584018c9ecb2ae8a05781be99
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-12-09 08:57:49 +0100

    rhrhzthtrr

--- lotto.py
+++ lotto.py
@@ -3,9 +3,10 @@ import random
 def lotto_game():
     print("Üdvözlünk a lottó játékban!")
     
-    jatekos_szamok = list(str(input("Adj meg 5 számot 1 és 90 között, vesszővel elválasztva: ")))
+    jatekos_szamok = list(str(input("Adj meg 5 számot 1 és 90 között, EGYBEN: ")))
 
-    nyero_szamok = random.sample(range(1, 91), 5)
+    # nyero_szamok = random.sample(range(1, 91), 5)
+    nyero_szamok = [1, 2, 3, 4, 5]
     
     korok = len(set(jatekos_szamok) & set(nyero_szamok))
     
